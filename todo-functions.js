6712576bef4d8580363a2b955b63d2e00464affe
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-06-27 12:26:16 +1000

    neatening code

--- todo-functions.js
+++ todo-functions.js
@@ -2,11 +2,8 @@
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
 
-    if (todosJSON !== null) {
-        return JSON.parse(todosJSON)
-    } else {
-        return []
-    }
+    return todosJSON !== null ? JSON.parse(todosJSON):[]
+    
 }
 
 // Save todos to localStorage
